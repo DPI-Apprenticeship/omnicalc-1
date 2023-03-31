88617f420e9a8707d61ec519df50b888a708a0f4
Meenoow Castillo <xcastillo33@gmail.com>
2023-03-31 16:16:32 +0000

    Roots form now functional. It is now linked to result and to try other numbers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   def blank_square_form
     render({ :template => "calculation_templates/square_form.html.erb" })
   end
+
+  def calculate_square
+    @num = params.fetch("number").to_i
+    @square_of_num = @num ** 2
+    render({ :template => "calculation_templates/square_results.html.erb"})
+
+  end  
 end
