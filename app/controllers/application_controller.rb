86f3432cbc4f5867b4dd4fa8d03398385331a526
Meenoow Castillo <xcastillo33@gmail.com>
2023-04-10 16:45:16 +0000

    New path to monthly payment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,13 +10,38 @@ class ApplicationController < ActionController::Base
   end
 
   def blank_random_form
-    render({ :template => "calculate_template/random_form.html.erb" })
+    render({ :template => "calculation_templates/random_form.html.erb" })
   end
 
   def calculate_random
     @lower = params.fetch("user_min").to_f
     @upper = params.fetch("user_max").to_f
-    @result = rand(@lower..@upper)
+    @result = rand(@lower..@upper) 
     render({ :template => "calculation_templates/random_results.html.erb" })
   end
+
+  def blank_payment_form
+    params[:apr] && params[:years] && params[:principal]
+    @apr = params[:apr].to_f
+    @years = params[:years]to_i
+    @principal = params[:principal].to_f
+
+   # @apr = params.fetch("apr").to_f
+    #@years = params.fetch("years_pay")to_i
+    #@principal = params.fetch("principal")to_f
+    #@result = fetch("monthly_payment")to_i
+
+    monthly_interest_rate = (@apr / 100) / 12
+    total_number_of_payments = @years * 12
+    numerator = monthly_interest_rate * (1 + monthly_interest_rate) ** total_number_of_payments
+    denominator = ((1 + monthly_interest_rate) ** total_number_of_payments) - 1
+    monthly_payment = @principal * (numerator / denominator).round(2)
+
+    render({ :template => "calculation_templates/payment_form.html.erb" }) 
+  end
+
+  def calculate_payment
+    render({ :template => "calculation_templates/monthly_payment.html.erb" }) 
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-
   get("/square/new", { :controller => "application", :action => "blank_square_form" })
 
   get("/square/results", { :controller => "application", :action => "calculate_square" })
 
-  get("/random/results", { :controller "application", => :action => "blank_random_form"})
+  get("/random/new", { :controller => "application", :action => "blank_random_form" })
 
   get("/random/results", { :controller => "application", :action => "calculate_random" })
 
+  get("/payment/new", { :controller => "application", :action => "blank_payment_form" })
+
+  get("/payment/results", { :controller => "application", :action => "calculate_payment"})
 end
